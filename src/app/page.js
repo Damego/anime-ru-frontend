b148dc9da75c32e4e70a8ee33cf243e7f4b6c2e6
Damego <danyabatueff@gmail.com>
2023-03-26 17:35:39 +0500

    refactor: test new components

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,8 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import Header from "@/app/components/header";
-import TitleCard from "@/app/components/titleCard";
+import Container from "@/app/components/container";
+import FilterMenu from "@/app/components/filterMenu";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -23,18 +24,91 @@ export default function Home() {
   //   wrapper()
   // }, [data, isLoaded, setData, setLoaded])
 
+  const titles = [
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+    {
+      imageUrl:
+        "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
+      name: "Ангел по соседству меня ужасно балует!",
+      url: "#",
+      score: 9.1,
+    },
+  ];
+
+  const onSortClick = (type) => {
+
+  }
+
   return (
     <main>
       <Header />
 
-      <TitleCard
-        imageUrl={
-          "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg"
-        }
-        titleName={"Ангел по соседству меня ужасно балует!"}
-        url={"#"}
-        score={9.1}
-      />
+      <div className={"flex flex-row ml-6 mt-10 mr-6 gap-4"}>
+        <Container titles={titles} />
+        <FilterMenu onSortClick={onSortClick}/>
+      </div>
     </main>
   );
 }
