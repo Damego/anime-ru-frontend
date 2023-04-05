a9e3dd9f55a6c6e5302d417371207e9e0314a4f5
Damego <danyabatueff@gmail.com>
2023-04-05 21:16:08 +0500

    feat(http): add login and register methods

--- src/app/api/httpClient.js
+++ src/app/api/httpClient.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 class HTTPClient {
   constructor() {
-    this.sessionID = ""
     this.instance = axios.create({
       baseURL: "http://127.0.0.1:8000/api/v1",
       withCredentials: true
@@ -14,7 +13,8 @@ class HTTPClient {
     const response = await this.instance.request({
       method,
       url: endpoint,
-      params
+      params,
+      data
     })
     return response.data;
   }
@@ -25,6 +25,24 @@ class HTTPClient {
 
   async getGenreList() {
     return this.request("GET", "/genres/list", {})
+  }
+
+  async register(login, email, password) {
+    const formData = new FormData();
+    formData.append("username", login);
+    formData.append("email", email);
+    formData.append("password", password)
+
+    return this.request("POST", "/users/", {data: formData});
+  }
+
+  async login(login, password) {
+    const formData = new FormData();
+    formData.append("username", login);
+    formData.append("password", password)
+    const data = await this.request("POST", "/users/login", {data: formData});
+
+    return data.session_id
   }
 }
 
