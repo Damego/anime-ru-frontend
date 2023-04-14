ff8cb3623b67a09c8e7011c576a191e75f60dc33
Damego <danyabatueff@gmail.com>
2023-04-14 21:35:19 +0500

    refactor(manage): use server genres

--- src/app/manage/page.js
+++ src/app/manage/page.js
@@ -1,21 +1,12 @@
 import httpClient from "../utils/httpClient"
 import Form from "./components/form"
 
-const predefinedGenres = [
-  {name: "Романтика", id: 1},
-  {name: "Иссекай", id: 2},
-  {name: "Фентези", id: 3},
-  {name: "Повседневность", id: 4},
-  {name: "Комедия", id: 5}
-]
-
 export default async function Manage() {
-    const genres = predefinedGenres.map(({name, id}) => ({name, value: id})) // await httpClient.getGenreList();
-
+    const genres = await httpClient.getGenreList();
 
     return (
         <div className="flex h-screen w-screen flex-col items-center justify-center">
-            <Form selectOptions={genres}/>
+            <Form selectOptions={genres.map(({name, id}) => ({name, value: id}))}/>
         </div>
     )
 }
