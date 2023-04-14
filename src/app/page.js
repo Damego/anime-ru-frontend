7439c8e3238cfe2ff4094b4d92a8e9584ab2d475
Damego <danyabatueff@gmail.com>
2023-04-14 21:36:15 +0500

    refactor(index): use server values; not predefined

--- src/app/page.js
+++ src/app/page.js
@@ -8,107 +8,25 @@ import httpClient from "@/app/utils/httpClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// Test data
-const titles = [
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-  {
-    imageUrl:
-      "https://animego.org/upload/anime/images/63f85d883089f370485553.jpg",
-    name: "Ангел по соседству меня ужасно балует!",
-    url: "#",
-    score: 9.1,
-  },
-];
-
-const predefinedGenres = [
-  {name: "Романтика", id: 1},
-  {name: "Иссекай", id: 2},
-  {name: "Фентези", id: 3},
-  {name: "Повседневность", id: 4},
-  {name: "Комедия", id: 5}
-]
-
 
 export default async function Home({searchParams}) {
   console.log(searchParams);
   const sessionID = cookies().get("session_id");
-  const genres = predefinedGenres //  await httpClient.getGenreList()
+  const genres = await httpClient.getGenreList()
   // I know we can use usememo hook but ehh...
   let selectedGenres = []
   if (searchParams.genres !== undefined) {
     const selectedGenreValues = searchParams.genres.split(',').map(strGenre => Number(strGenre));
     selectedGenres = genres.filter(genre => selectedGenreValues.includes(genre.id));    
   }
+  const {sort, search} = searchParams;
 
-  // I'm not at home rn so I don't have good pc to run both front and back
-  const animeTitles = titles // await httpClient.getAnimeList()
-  const user = null // await httpClient.getMe();
+  const animeTitles = await httpClient.getAnimeList(sort, selectedGenres.map(genre => genre.id).toString(), search)
+  // const user = await httpClient.getMe();
 
   return (
     <main>
-      <Header username={user?.name}/>
+      <Header username={undefined}/>
 
       <div className={"ml-6 mt-10 mr-6 flex flex-row gap-4"}>
         <Container titles={animeTitles} />
