a7d7b00cd5c0aadc89eb8a44c650beb6d1167e32
Damego <danyabatueff@gmail.com>
2023-04-14 21:36:52 +0500

    refactor(manage): change state on input

--- src/app/manage/components/form.jsx
+++ src/app/manage/components/form.jsx
@@ -7,6 +7,8 @@ import httpClient from "@/app/utils/httpClient";
 export default function Form({ selectOptions }) {
   const [selectedOptions, changeSelectedOptions] = useState([]);
   const [file, changeFile] = useState();
+  const [animeName, setAnimeName] = useState(null);
+  const [animeDescription, setAnimeDescription] = useState(null);
 
   const onOptionSelect = (value) => {
     const [option] = selectOptions.filter(option => option.value === value)
@@ -24,25 +26,24 @@ export default function Form({ selectOptions }) {
    * @param {File} file 
    */
   const onFileChange = (file) => {
-    // TODO: check for non png file types
-    console.log(file.type);
+    changeFile(file)
   }
 
   const onButtonClick = async () => {
-    await httpClient.addAnime()
+    await httpClient.addAnime(animeName, animeDescription, selectedOptions.map(option => option.value), file)
   }
 
   return (
     <div className="w-1/3 h-1/2 flex flex-col items-center justify-center rounded-lg bg-white text-xl shadow-md">
       <label>Изображение</label>
-      <input type="file" accept="image/png" name="image" onChange={formElement => onFileChange(formElement.target.value)}/>
+      <input type="file" accept="image/png" name="image" onChange={formElement => onFileChange(formElement.target.files[0])}/>
       <div>
         <label>Название</label>
-        <input className="border-2" />
+        <input className="border-2" onChange={inputField => setAnimeName(inputField.target.value)}/>
       </div>
       <div>
         <label>Описание</label>
-        <input className="border-2" />
+        <input className="border-2" onChange={inputField => setAnimeDescription(inputField.target.value)}/>
       </div>
       <div>
         <label>Жанры</label>
