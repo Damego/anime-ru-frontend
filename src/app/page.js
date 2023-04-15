9b9a5968b27c39c3cb390b4125aa74da4c322f8a
Damego <danyabatueff@gmail.com>
2023-04-15 17:24:03 +0500

    fix: finally fix cookies don't send on any request

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,21 @@
-import {cookies} from "next/headers";
+import { cookies } from "next/headers";
 import { Inter } from "next/font/google";
 import Header from "@/app/components/header";
 import Container from "@/app/components/container";
 import FilterMenu from "@/app/components/filterMenu";
 import httpClient from "@/app/utils/httpClient";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
+export default async function Home({ searchParams }) {
+  const cookie = cookies().get("session_id");
+  // I hate this shit named server components
+  httpClient.instance.defaults.headers.common[
+    "Cookie"
+  ] = `${cookie.name}=${cookie.value}`;
+  const user = await httpClient.getMe();
 
-export default async function Home({searchParams}) {
-  console.log(searchParams);
-  const sessionID = cookies().get("session_id");
-  const genres = await httpClient.getGenreList()
+  const genres = await httpClient.getGenreList();
   // I know we can use usememo hook but ehh...
   let selectedGenres = []
   if (searchParams.genres !== undefined) {
